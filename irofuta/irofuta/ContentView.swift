48cff2fd7994622880e1d6159255b377aea5cd19
Geo-Jagaimo <salamanderezo@gmail.com>
2026-09-15 19:13:06 +0900

    Create iOS app with Xcode

---
+++ irofuta/irofuta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  irofuta
+//
+//  Created by Keita UEMORI on 2026/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome irofuta App!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ irofuta/irofuta/irofutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  irofutaApp.swift
+//  irofuta
+//
+//  Created by Keita UEMORI on 2026/09/15.
+//
+
+import SwiftUI
+
+@main
+struct irofutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
